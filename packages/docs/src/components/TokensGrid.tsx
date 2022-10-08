880a7e9ad9b7c8750d282944621c3f27e1a41fec
Caio Tracera <ctrcra@gmail.com>
2022-10-08 00:21:30 -0300

    feat: initial commit

---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,27 @@
+import '../styles/tokens-grid.css'
+
+interface TokensGridProps {
+  tokens: Record<string, string>
+  hasRemValues?: boolean
+}
+
+export function TokensGrid({ tokens, hasRemValues = false }: TokensGridProps) {
+  return (
+    <table className="tokens-grid">
+      <thead>
+        <th>Name</th>
+        <th>Value</th>
+        {hasRemValues && <th>Pixels</th>}
+      </thead>
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{value}</td>
+            {hasRemValues && <td>{Number(value.replace('rem', '')) * 16}px</td>}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
